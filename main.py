9974cbfb0bf6fbdf4c1b9801b6fbb03a8f11680c
Gomazkow <opypartur@gmail.com>
2025-12-03 17:17:44 +0300

    commit

--- main.py
+++ main.py
@@ -1,43 +1,38 @@
 from contextlib import asynccontextmanager
 
-from pydantic import BaseModel
-from fastapi import FastApi
-from fastapi.middleware.cord import CORSmiddleware
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from crud import add_user, get_tasks, get_completed_tasks_count
 from models import init_db
-import requests as rq
 
 
 @asynccontextmanager
-async def lifespan(app_:FastApi):
+async def lifespan(app_: FastAPI):
     await init_db()
-    print('Bot is Ready')
+    print("Bot is Ready")
     yield
 
 
-
-app = FastApi(title="To Do APP",lifespan=lifespan)
-
-
+app = FastAPI(title="To Do APP", lifespan=lifespan)
 
 app.add_middleware(
-    CORSmiddleware,
-    allow_origins=['*'],
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers = ["*"],
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
-
 @app.get("/api/tasks/{tg_id}")
-async def tasks(tg_id:int):
-    user = await rq.add_user(tg_id)
-    return await rq.get_tasks(user.id)
+async def tasks(tg_id: int):
+    user = await add_user(tg_id)
+    return await get_tasks(user.id)
 
 
 @app.get("/api/main/{tg_id}")
-async def profile(tg_id:int):
-    user  = await rq.add_user(tg_id)
-    completed_task_count = await rq.get_compited_tasks_count(user.id)
-    return {'completedTasks': completed_task_count}
+async def profile(tg_id: int):
+    user = await add_user(tg_id)
+    completed_task_count = await get_completed_tasks_count(user.id)
+    return {"completedTasks": completed_task_count}
--- models.py
+++ models.py
@@ -1,38 +1,36 @@
-from sqlalchemy import ForeignKey,String,BigInteger
-from sqlalchemy.orm import Mapped,DeclarativeBase,mapped_column
-from sqlalchemy.ext.asyncio import AsyncAttrs,async_sessionmaker, create_async_engine
+from sqlalchemy import ForeignKey, String, BigInteger
+from sqlalchemy.orm import Mapped, DeclarativeBase, mapped_column, relationship
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 
-engine = create_async_engine(url="sqlite+aiosqlite:///db.sqlite3", echo = True)
-
-
-async_session = async_sessionmaker(bind = engine, expire_on_commit = False)
-
-
-
+engine = create_async_engine(url="sqlite+aiosqlite:///db.sqlite3", echo=True)
+async_session = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 
 class Base(AsyncAttrs, DeclarativeBase):
     pass
 
+
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key = True)
-    tg_id: Mapped[int]  = mapped_column(BigInteger)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tg_id: Mapped[int] = mapped_column(BigInteger, unique=True, index=True)
 
-
+    # связь с задачами (не обязательно, но полезно)
+    tasks: Mapped[list["Task"]] = relationship(back_populates="user")
 
 
 class Task(Base):
-    __tablename__ = 'users'
-    id: Mapped[int] = mapped_column(primary_key = True)
-    title: Mapped[int]  = mapped_column(String(128))
-    completed:Mapped[bool] = mapped_column(default = False)
-    user:Mapped[int] = mapped_column(ForeignKey('usrs.id',ondelete='Cascade'))
+    __tablename__ = "tasks"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(128))
+    completed: Mapped[bool] = mapped_column(default=False)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), index=True)
+    user: Mapped["User"] = relationship(back_populates="tasks")
 
 
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-
-
